0524e1a84a94db959af9230d1e577c052838fb9c
sam653986 <81320212+sam653986@users.noreply.github.com>
2021-05-22 22:57:18 +0700

    Add files via upload

---
+++ minecraft on Ursina.py.py
@@ -0,0 +1,14 @@
+#лрестики нолики
+
+import random
+
+def drawBoard(board):
+    # function
+
+    #board
+    print(board[7] + '|' + board[8] + '|' + board[9])
+    print('-+-+-')
+    print(board[4] + '|' + board[5] + '|' + board[6])
+    print('-+-+-')
+    print(board[1] + '|' + board[2] + '|' + board[3])
+
